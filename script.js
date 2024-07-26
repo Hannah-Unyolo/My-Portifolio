e8d086dad6635128d212dc62e64f1d50c369b81e
Hannah-Unyolo <unyolohannah@gmail.com>
2024-07-26 17:42:14 +0300

    added google analytics

--- script.js
+++ script.js
@@ -41,12 +41,12 @@ $(document).ready(function(){
         loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Software Developer", "UX/UI Designer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // var typed = new Typed(".typing-2", {
+    //     strings: ["Software Developer", "UX/UI Designer"],
+    //     typeSpeed: 100,
+    //     backSpeed: 60,
+    //     loop: true
+    // });
 
     // owl carousel script
     $('.carousel').owlCarousel({
